17b43374a2d330ab291aa3ff76e6c340a0363945
mbuyco <buyco.michael@gmail.com>
2022-04-04 00:02:20 +0800

    update bash script

--- notes.sh
+++ notes.sh
@@ -2,21 +2,28 @@
 
 set -euo pipefail
 
+# Note paths
 NOTES_FILENAME="notessh-$(date +%Y%m%d).txt"
+NOTES_PATH="$HOME/notes"
+ACTIVE_NOTE_PATH="$NOTES_PATH/$NOTES_FILENAME"
 
-if [ ! -d ~/notes ]; then
-  mkdir ~/notes
+# Create notes folder if not exists
+if [ ! -d $NOTES_PATH ]; then
+  mkdir $NOTES_PATH
 fi
 
-if [ ! -e ~/notes/$NOTES_FILENAME ]; then
-  touch ~/notes/$NOTES_FILENAME
+# Create active note file if not exists
+if [ ! -e $ACTIVE_NOTE_PATH ]; then
+  touch $ACTIVE_NOTE_PATH
 fi
 
-cd ~/notes
-which vim 2&>1 /dev/null
-
-if [ $? -eq 1 ]; then
-  echo "ERROR: Vim is not installed"
+# Run CLI text editor (neovim, vim or nano)
+if command -v nvim &> /dev/null
+then
+  nvim $ACTIVE_NOTE_PATH
+elif command -v vim &> /dev/null
+then
+  vim $ACTIVE_NOTE_PATH
 else
-  vim ~/notes/$NOTES_FILENAME
+  nano $ACTIVE_NOTE_PATH
 fi
